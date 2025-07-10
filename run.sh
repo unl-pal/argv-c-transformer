a9a3b4bac60d3de3dd93877ef551c28dcb4e0ab1
jckreg <jonathancraig@boisestate.edu>
2025-07-10 10:20:16 -0600

    need to figure out the remove empty crap situation

--- run.sh
+++ run.sh
@@ -36,3 +36,5 @@ echo "=================================== Run Filter ===========================
 
 echo "=================================== Run Transform ==================================="
 ./build/transform "$configFile"
+
+# find benchmark -empty -delete
