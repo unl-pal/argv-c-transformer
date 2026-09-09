72bed32ad40a6fbe9039c45937144e14a2de166c
natsteven <nathanaelsteven@u.boisestate.edu>
2026-09-09 14:28:37 -0600

    no more bare clang
    - checkCompilable/preprocess in Verifier.cpp now run
    clang::SyntaxOnlyAction/clang::PrintPreprocessedAction in-process via a
    new runFrontendActionCheckingErrors
    - Removed checkRuntimeClangVersion, clangCommand, and shellQuote

--- src/full/main.cpp
+++ src/full/main.cpp
@@ -15,7 +15,6 @@
 
 int main(int argc, char **argv) {
   checkClangVersion();
-  checkRuntimeClangVersion();
   std::optional<CliInvocation> invocation = parseCliArgs(argc, argv);
   if (!invocation) {
     printUsage("argv-c");
